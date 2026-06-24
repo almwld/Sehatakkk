c60608acd70afde052d725a65e074e699a9f3404
almwld <almwld@github.com>
2026-06-24 05:38:43 +0300

    🖼️ إضافة الأيقونة الصحيحة (Sehatak-icon.png) إلى المشروع

--- sehatak_flutter/android/app/src/main/kotlin/com/sehatak/sehatak/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.sehatak.app
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
--- sehatak_flutter/lib/main_background.dart
+++
@@ -1,8 +0,0 @@
-// أضف هذا في main.dart:
-// import 'core/services/background_service.dart';
-// 
-// في دالة main():
-// BackgroundSyncService.start();
-// 
-// عند الخروج:
-// BackgroundSyncService.stop();
